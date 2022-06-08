96007b70b024ef8890a33cbe31ad8ab35e7135fe
J.V Krakowski <krakowski.jv@gmail.com>
2022-06-07 23:13:17 -0400

    Edit

--- sorti.js
+++ sorti.js
@@ -1,12 +1,12 @@
 $(function (){
-    $(".tab-nav button:not(:first)").addClass("inactive");
+    $(".tab-nav a:not(:first)").addClass("inactive");
     $(".tab-panel").hide();
     $(".tab-panel:first").show();
 
-    $(".tab-nav button").click(function(){
+    $(".tab-nav a").click(function(){
         var tabs = $(this).attr("id");
         if( $(this).hasClass("inactive") ) {
-            $(".tab-nav button").addClass("inactive");
+            $(".tab-nav a").addClass("inactive");
             $(this).removeClass("inactive");
             
             $(".tab-panel").hide();
