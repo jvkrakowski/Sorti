df61bc5898f788f39171e1ba99d13363e984b41c
judia-waverly <krakowski.jv@gmail.com>
2024-07-30 12:20:13 -0400

    Updated code
    Updated for clean, efficient code with the proper error fixes.

--- sorti.js
+++ sorti.js
@@ -1,18 +1,28 @@
-$(function (){
+$(function () {
     $(".tab-nav a:not(:first)").addClass("inactive");
     $(".tab-panel").hide();
     $(".tab-panel:first").show();
 
-    $(".tab-nav a").click(function(){
+    $(".tab-nav button").click(function () {
         var tabs = $(this).attr("id");
-        if( $(this).hasClass("inactive") ) {
-            $(".tab-nav a").addClass("inactive");
+        if ($(this).hasClass("inactive")) {
+            $(".tab-nav button").addClass("active");
             $(this).removeClass("inactive");
-            
+
             $(".tab-panel").hide();
             $("#" + tabs + "-panel").fadeIn("slow");
         }
 
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this).addClass("inactive");
+
+            $(".tab-panel").hide();
+            $("#" + tabs + "-panel").fadeIn("slow");
+        }
+
+
+
     });
-    
+
 });
